143de5cf469c5e4351465b92d02f3cb67d367ea5
vidushidwivedi <vidushidwivedi31@gmail.com>
2024-02-16 16:28:46 +0530

    Added a program of datatypes.cpp

---
+++ Step1_Basics/Step1.1_Intro_of_Cpp/datatypes.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int dataTypes(string type) {
+	// Write your code here
+	if(type=="Integer"){
+		return 4;
+	}
+	else if(type=="Long"){
+		return 8;
+	}
+	else if(type=="Float"){
+		return 4;
+	}
+	else if(type=="Double"){
+		return 8;
+	}
+	else if(type=="Character"){
+		return 1;
+	}
+
+}
