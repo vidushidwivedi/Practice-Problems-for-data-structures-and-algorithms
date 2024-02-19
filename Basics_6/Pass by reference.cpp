4606b8e2d4388a57d9b225053048688017a4520b
vidushidwivedi <vidushidwivedi31@gmail.com>
2024-02-19 11:30:06 +0530

    Added Basics 6 folder

---
+++ Basics_6/Pass by reference.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void incre_ment(int &n, int n1){
+		n++;
+		n1++;
+		
+}
+int main(){
+		int a=3;
+		int b=5;
+		cout<<a<<" "<<b<<endl;
+		incre_ment(a, b);
+		cout<<a<<" "<<b<<endl;
+	}
