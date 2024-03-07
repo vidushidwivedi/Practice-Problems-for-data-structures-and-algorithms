938f45db260b52fbd4b4f3253dc03ad6c7f6f574
vidushidwivedi <vidushidwivedi31@gmail.com>
2024-03-07 17:44:10 +0530

    Added a pattern program for 4 lines and 4 columns with no symmetry

---
+++ Patterns/Pattern1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void print1(){
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
+
+
+
+int main(){
+	print1();
+}
