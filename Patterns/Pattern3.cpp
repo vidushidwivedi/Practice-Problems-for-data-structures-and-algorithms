05e5c9db32d2c9844a91a494433ed9484c982f22
vidushidwivedi <vidushidwivedi31@gmail.com>
2024-03-11 21:38:18 +0530

    Added pattern 3 program

---
+++ Patterns/Pattern3.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void print1(int n){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
+void print2(int n){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<=i; j++){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+}
+
+void print3(int n){
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=i; j++){
+			cout<<j<<" ";
+		}
+		cout<<endl;
+	}
+}
+
+int main(){ 
+	int t;
+	cin>>t;
+	for(int i=0; i<t; i++){
+		int n;
+		cin>>n;
+		print3(n);
+
+	}
+}
