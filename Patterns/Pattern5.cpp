35cca5b2100cfde448d0c6f0acce8f99e98e30c5
vidushidwivedi <vidushidwivedi31@gmail.com>
2024-03-12 13:16:12 +0530

    Added pattern 5 program

---
+++ Patterns/Pattern5.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void print1(int n){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
+void print2(int n){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<=i; j++){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+}
+
+void print3(int n){
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=i; j++){
+			cout<<j<<" ";
+		}
+		cout<<endl;
+	}
+}
+ 
+void print4(int n){
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=i; j++){
+			cout<<i<<" ";
+		}
+		cout<<endl;
+	}
+}
+
+void print5(int n){
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=n-i+1;j++){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+}
+
+
+int main(){ 
+	int t;
+	cin>>t;
+	for(int i=0; i<t; i++){
+		int n;
+		cin>>n;
+		print5(n);
+
+	}
+	return 0;
+}
