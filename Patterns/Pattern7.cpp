a38b99756e8a710fd4a6f526be316966843698f0
vidushidwivedi <vidushidwivedi31@gmail.com>
2024-04-04 22:40:19 +0530

    Updated code for Pattern 7

---
+++ Patterns/Pattern7.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print1(int n){
+	for(int i=0; i<n; i++){
+		for (int j=0; j<n; j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
+
+void print2(int n){
+	for (int i=0; i<n; i++){
+		for (int j=0; j<i+1; j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
+
+void print3(int n){
+	for (int i=1; i<n+1; i++){
+		for (int j=1; j<i+1; j++){
+			cout<<j;
+		}
+		cout<<endl;
+	}
+}
+
+void print4(int n){
+	for (int i=1; i<n+1; i++){
+		for (int j=1; j<i+1; j++){
+			cout<<i;
+		}
+		cout<<endl;
+	}
+}
+
+void print5(int n){
+	for (int i=1; i<=n; i++){
+		for (int j=0; j<n-i+1; j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
+
+void print6(int n){
+	for (int i=1; i<=n; i++){
+		for (int j=1; j<=n-i+1; j++){
+			cout<<j;
+		}
+		cout<<endl;
+	}
+}
+
+void print7(int n){
+	for(int i=0; i<=n; i++){
+		for (int j=0; j<n-i-1; j++){
+			cout<<" ";
+		}
+
+		for (int k=0; k<2*i+1; k++){
+			cout<<"*";
+		}
+
+		for (int j=0; j<n-i-1; j++){
+			cout<<" ";
+		}
+		cout<<endl;
+	}
+}
+
+int main(){
+	int t;
+	cin>>t;
+	for (int i=0; i<t;i++){
+		int n; 
+		cin >> n;
+		print7(n);
+	}
+	
+	
+}
