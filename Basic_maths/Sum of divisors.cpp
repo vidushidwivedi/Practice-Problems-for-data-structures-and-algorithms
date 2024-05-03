d6a99af2ee44aeacce0a9dde9f56a1edc7d72f91
vidushidwivedi <vidushidwivedi31@gmail.com>
2024-05-03 12:06:14 +0530

    Updated program for printing n times in recursion

--- Basic_maths/Sum of divisors.cpp
+++ Basic_maths/Sum of divisors.cpp
@@ -1,10 +1,16 @@
+//brute force approach
 int sumOfAllDivisors(int n){
-	int sum=0;
-
-	for(int i=0;i<=n;i++){
-			if(n%i==0){
-				sum+=i;
-			}
+	long long result=0;
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=i; j++){
+			if(i%j==0){
+			result+=j;
+		}
 	}
-	return sum;
+		
+	}
+	return result;
 }
+
+//optimal approach
+
--- Recursion/Print 1 to N.cpp
+++ Recursion/Print 1 to N.cpp
@@ -20,3 +20,18 @@ int main(){
   return 0;
 
 }
+
+//codingninjas problem solution 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// vector<string> printNTimes(int n) {
+// 	vector<string> result;
+// 	if(n==0){
+// 		return result;
+// 	}
+// 	result= printNTimes(n-1);
+// 	result.push_back("Coding Ninjas");
+// 	return result;
+
+// }
