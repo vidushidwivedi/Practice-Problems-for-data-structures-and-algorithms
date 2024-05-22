deb6542b2a5b0a1cecf37b4bc4e4141dbd4c55cf
vidushidwivedi <vidushidwivedi31@gmail.com>
2024-05-22 22:50:02 +0530

    best approach code

--- Arrays/majority_elements_greater_than_n_divideby_2.cpp
+++ Arrays/majority_elements_greater_than_n_divideby_2.cpp
@@ -36,3 +36,26 @@ public:
         return -1;
 }
 };
+
+// ****optimal solution**** //
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int el;
+        int count=0;
+        for(int i=0; i<nums.size(); i++){
+            if(count==0){
+                count=1;
+                el= nums[i];
+            }
+            else if(nums[i]==el){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return el;
+    }
+};
