c8907080af7606333faadcc4cdee41543d02274b
vidushidwivedi <vidushidwivedi31@gmail.com>
2024-08-11 06:25:41 +0530

    Modified the code for pattern 7

--- Patterns/Pattern7.cpp
+++ Patterns/Pattern7.cpp
@@ -56,16 +56,16 @@ void print6(int n){
 }
 
 void print7(int n){
-	for(int i=0; i<=n; i++){
-		for (int j=0; j<n-i-1; j++){
+	for(int i=1; i<=n; i++){
+		for (int j=1; j<n-i+1; j++){
 			cout<<" ";
 		}
 
-		for (int k=0; k<2*i+1; k++){
+		for (int j=2; j<(2*i+1); j++){
 			cout<<"*";
 		}
 
-		for (int j=0; j<n-i-1; j++){
+		for (int j=1; j<n-i+1; j++){
 			cout<<" ";
 		}
 		cout<<endl;
