dbdf1f7c21261e21ca820cfcf717818ac400e6d3
vidushidwivedi <vidushidwivedi31@gmail.com>
2024-08-11 06:29:59 +0530

    modified the previous codes for pattern 8

--- Patterns/Pattern8.cpp
+++ Patterns/Pattern8.cpp
@@ -56,22 +56,21 @@ void print6(int n){
 }
 
 void print7(int n){
-	for(int i=0; i<=n; i++){
-		for (int j=0; j<=n-i-1; j++){
+	for(int i=1; i<=n; i++){
+		for (int j=1; j<n-i+1; j++){
 			cout<<" ";
 		}
 
-		for (int k=0; k<(2*i+1); k++){
+		for (int j=2; j<(2*i+1); j++){
 			cout<<"*";
 		}
 
-		for (int j=0; j<=n-i-1; j++){
+		for (int j=1; j<n-i+1; j++){
 			cout<<" ";
 		}
 		cout<<endl;
 	}
 }
-
 void print8(int n){
 	for(int i=0; i<n; i++){
 		for (int j=0; j<i; j++){
