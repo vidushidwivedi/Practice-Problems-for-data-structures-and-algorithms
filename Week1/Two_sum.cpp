d9e4a95ea2b21c84116ad8805b5a02e258628c99
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-05-06 10:01:20 +0530

    Added the better approaches for Two Sum Problem

--- Week1/Two_sum.cpp
+++ Week1/Two_sum.cpp
@@ -14,3 +14,44 @@ public:
         return {};
     }
 };
+
+//Two pass solution (better approaches ahead)
+
+// class Solution {
+//     public:
+//         vector<int> twoSum(vector<int>& nums, int target) {
+//             unordered_map <int, int> NumsMap;
+//             int n= nums.size();
+            
+//             for(int i=0; i<n; i++){
+//                 NumsMap[nums[i]]=i;
+//             }
+    
+//             for (int i=0; i< n; i++){
+//                 int complement= target - nums[i];
+//                 if(NumsMap.count(complement) && NumsMap[complement] != i){
+//                     return {i, NumsMap[complement]};
+//                 }
+//             }
+//             return {};
+//         }
+//     };
+
+//One pass solution (better approach)
+
+// class Solution {
+//     public:
+//         vector<int> twoSum(vector<int>& nums, int target) {
+//             unordered_map <int, int> NumsMap;
+//             int n= nums.size();
+    
+//             for (int i=0; i<n; i++){
+//                 int complement= target- nums[i];
+//                 if (NumsMap.count(complement)){
+//                     return {i, NumsMap[complement]};
+//                 }
+//                  NumsMap[nums[i]]= i;
+//             }
+//             return {};
+//         }
+//     };
