0e335b1093ec707327c3b741377b032218c2528f
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-07-28 12:37:06 +0530

    Modified the code with local compatible syntax

--- Week1/Two_sum.cpp
+++ Week1/Two_sum.cpp
@@ -1,19 +1,51 @@
-#include <vector>  // Required for std::vector
+#include <bits/stdc++.h>
+using namespace std;
 
-class Solution {
+class Solution { //class
 public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        int n = nums.size();
-        for (int i = 0; i <= n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
-            }
-        }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); ++i)
+            for (int j = i + 1; j < nums.size(); ++j)
+                if (nums[i] + nums[j] == target)
+                    return {i,j};
         return {};
     }
 };
+//creating an object of the class Solution and calling the function twoSum
+//Solution s;
+// s.twoSum(nums, target)
+
+int main() {
+    vector<int> nums{2, 7, 11, 15};
+    int target = 9;
+    
+    Solution s;
+    vector<int> result= s.twoSum(nums, target);
+    for (int i=0; i<result.size(); i++){
+        cout << result[i]<< " ";
+    }
+    // for (int i : Solution().twoSum(nums, target))
+    //     cout << i << " ";
+    cout << endl;
+}
+
+
+// #include <vector>  // Required for std::vector
+
+// class Solution {
+// public:
+//     std::vector<int> twoSum(std::vector<int>& nums, int target) {
+//         int n = nums.size();
+//         for (int i = 0; i <= n - 1; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     return {i, j};
+//                 }
+//             }
+//         }
+//         return {};
+//     }
+// };
 
 //Two pass solution (better approaches ahead)
 
