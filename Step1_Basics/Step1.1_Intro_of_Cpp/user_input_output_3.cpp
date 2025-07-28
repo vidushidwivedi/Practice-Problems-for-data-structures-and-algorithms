0548b7cbc27907a16bdbebacf351606061f1baa7
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-07-28 13:17:17 +0530

    Added new code for input and output

---
+++ Step1_Basics/Step1.1_Intro_of_Cpp/user_input_output_3.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void printNumber() { 
+        int n; 
+        cout <<"(user gives value):";
+        cin>>n;
+        cout<< "You entered: " << n << endl;    }
+};
+
+int main(){
+    Solution s;
+    s.printNumber();
+    return 0;
+}
