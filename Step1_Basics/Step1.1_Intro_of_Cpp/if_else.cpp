5e30cd0151efcae7896748ba372dfb51c7c12059
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-07-28 15:52:39 +0530

    Modified with new code

--- Step1_Basics/Step1.1_Intro_of_Cpp/if_else.cpp
+++ Step1_Basics/Step1.1_Intro_of_Cpp/if_else.cpp
@@ -12,7 +12,35 @@ string compareIfElse(int a, int b) {
 	else if (a==b){
 		return "equal";
 	}
+	return "";
 }
 
+void studentGrade (int marks){
+	if (marks >= 90){
+		cout << "Grade A" << endl;
+	}
+	else if (marks >= 70){
+		cout << "Grade B" << endl;
+	}
+	else if (marks >= 50){
+		cout << "Grade C" <<endl;
+	}
+		else if (marks >= 35){
+		cout << "Grade D" <<endl;
+	}
+	else {
+		cout << "Fail" << endl;
+	}
+}
 
-
+int main(){
+	int a,b;
+	cout<<"Enter two numbers: ";
+	cin>>a>>b;
+	cout<<compareIfElse(a,b)<< endl;
+	int marks;
+	cout << "Enter marks: ";
+	cin >> marks;
+	studentGrade(marks);
+	return 0;
+}
