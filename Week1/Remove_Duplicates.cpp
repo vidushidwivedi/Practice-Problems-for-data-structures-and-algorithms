03eba89425e0c34d4ec291c6c7feb24a2f1b6a21
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-07-29 11:50:13 +0530

    Added the main function and helper function

--- Week1/Remove_Duplicates.cpp
+++ Week1/Remove_Duplicates.cpp
@@ -1,30 +1,69 @@
 //Problem: 83 Remove Duplicates from Sorted List
 // Link: https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
-#include <vector>  // Required for std::vector
-#include <unordered_map>  // Required for std::unordered_map
-#include <iostream>  // Required for std::cout and std::endl
 
- //Definition for singly-linked list.
-  struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
- };
- 
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 class Solution {
-    public:
-        ListNode* deleteDuplicates(ListNode* head) {
-            ListNode* res= head;
-            while (head && head->next ){
-                if(head->val == head->next->val){
-                    head->next= head->next->next;
-                }else{
-                    head= head->next;
-                }
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* res = head;
+        while (head && head->next) {
+            if (head->val == head->next->val) {
+                head->next = head->next->next;
+            } else {
+                head = head->next;
             }
-            return res;
         }
-    };
+        return res;
+    }
+};
+
+// Function to print the linked list
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+// Function to create a linked list from a vector
+ListNode* createList(const vector<int>& nums) {
+    if (nums.empty()) return nullptr;
+    ListNode* head = new ListNode(nums[0]);
+    ListNode* current = head;
+    for (int i = 1; i < nums.size(); ++i) {
+        current->next = new ListNode(nums[i]);
+        current = current->next;
+    }
+    return head;
+}
+
+int main() {
+    // Example input: sorted list with duplicates
+    vector<int> input = {1, 1, 2, 3, 3};
+
+    // Create list
+    ListNode* head = createList(input);
+    
+    cout << "Original list: ";
+    printList(head);
+
+    // Remove duplicates
+    Solution solution;
+    ListNode* newHead = solution.deleteDuplicates(head);
+
+    cout << "List after removing duplicates: ";
+    printList(newHead);
+
+    return 0;
+}
