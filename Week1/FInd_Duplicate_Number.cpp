4e19ad13df0852185d511866b50e809904285586
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-07-29 19:19:28 +0530

    Added a question for finding the duplicate number

---
+++ Week1/FInd_Duplicate_Number.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        int findDuplicate(vector<int> & nums){
+            int left= nums[0];
+            int right= nums[1];
+            while (left != right){
+            }
+        }
+};
