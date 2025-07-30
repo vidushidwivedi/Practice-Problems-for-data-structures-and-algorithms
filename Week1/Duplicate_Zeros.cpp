8022b21f385573f6dd731453dc27184bb4cc73e8
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-07-30 20:52:02 +0530

    Made some changes in the code of adding duplicate zeros

--- Week1/Duplicate_Zeros.cpp
+++ Week1/Duplicate_Zeros.cpp
@@ -3,7 +3,28 @@ using namespace std;
 
 class Solution{
     public:
-        void Duplicatezeros(vector<int>& arr){
-            
+        void duplicateZeros(vector<int>& arr){
+            int zeros= 0;
+            for (int i=0; i<arr.size(); i++){
+                if (arr[i]==0){
+                    zeros++;
+                }
+            }
+            int i= arr.size()-1;
+            int j= arr.size()+ zeros-1;
+            while (i!=j){
+                if (j < arr.size()) {
+                    arr[j] = arr[i]; // Shift current value
+                }
+                if (arr[i]==0){
+                    j--;
+                    if (j < arr.size()) {
+                        arr[j] = 0; // Duplicate zero
+                    }
+                }
+                i--;
+                j--;
+            }
         }
+
 };
