ad21669957de29669f4b08cdd8577b71e6f0ae3d
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-07-30 19:44:41 +0530

    Added entire code for finding the duplicate number using FLoyd's Algo

--- Week1/FInd_Duplicate_Number.cpp
+++ Week1/FInd_Duplicate_Number.cpp
@@ -4,9 +4,24 @@ using namespace std;
 class Solution {
     public:
         int findDuplicate(vector<int> & nums){
-            int left= nums[0];
-            int right= nums[1];
-            while (left != right){
-            }
-        }
+            int slow= nums[0];
+            int fast= nums[0];
+            
+            //find the intersection of the cycle
+            do{
+                slow= nums[slow];
+                fast= nums[nums[fast]];
+            } while (slow!= fast);
+
+
+        //find the starting of the cycle
+        int slow2= nums[0]; //another 
+        do{
+            slow= nums[slow];
+            slow2= nums[slow2];
+        } while (slow!=slow2);
+
+        return slow;
+
+    }
 };
