a8806ffabc763d2bd1714af0434dd8dbc91d73e0
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-07-31 12:34:55 +0530

    Added new code for three sum smaller

---
+++ Week1/3Sum_Smaller.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @param nums:  an array of n integers
+     * @param target: a target
+     * @return: the number of index triplets satisfy the condition nums[i] + nums[j] + nums[k] < target
+     */
+    int threeSumSmaller(vector<int> &nums, int target) {
+        // Write your code here
+    }
+};
