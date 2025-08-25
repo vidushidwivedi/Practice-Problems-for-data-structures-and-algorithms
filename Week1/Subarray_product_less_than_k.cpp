76543eb45f96c54e21f6570f3184e85d2bd26411
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-08-25 13:25:40 +0530

    Added sliding window approach with suitable comments

--- Week1/Subarray_product_less_than_k.cpp
+++ Week1/Subarray_product_less_than_k.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 //BRUTE FORCE
-int numSubarrayProductLessThanK (vector<int> & nums, int k){
-    int n= nums.size();
-    int count= 0; //in order to show the count of the arrays
+// int numSubarrayProductLessThanK (vector<int> & nums, int k){
+//     int n= nums.size();
+//     int count= 0; //in order to show the count of the arrays
 
-    //generating every possible subarray
-    for (int i=0; i<n; i++){
-        for (int j=i; j<n; j++){
-            long long product= 1;
-            //compute the product of subarray nuns[i..j]
-            for (int p=i; p<=j; p++){
-                product*=nums[p];
-            }
-            if (product<k){
-                count++;
-            }
-        }
-    }
-    return count;
-}
+//     //generating every possible subarray
+//     for (int i=0; i<n; i++){
+//         for (int j=i; j<n; j++){
+//             long long product= 1;
+//             //compute the product of subarray nuns[i..j]
+//             for (int p=i; p<=j; p++){
+//                 product*=nums[p];
+//             }
+//             if (product<k){
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// }
 //tc= o(n^3)
 
 // BETTER SOLUTION
@@ -38,6 +38,35 @@ int numSubarrayProductLessThanK (vector<int> & nums, int k){
 //     }
 // }
 //tc= o(n^2)
+
+
+//Sliding window approach
+int numSubarrayProductLessThanK (vector<int> & nums, int k){
+    if (k<=1){
+        return 0;
+    }
+    int n= nums.size();
+    int left= 0;
+    int count= 0;
+    long long product= 1;
+
+    //window ke andr jo elements honge unhe multiply krungi: ek ek karke right ptr element ko multiply krti rhugi(i.e, contiguous element)
+    for (int right=0; right<n; right++){
+        product*= nums[right];
+
+        //agar condition match nahi hoti hai toh mein window ko shrink karungi left forward direction se 
+        //imp: use while instead of if becoz hume continuously use shrink krna hai jab tak window ki condition satisfy nahi ho jati
+        while (right>left && product>=k ){
+            //shrink krugi
+            product /= nums[left];
+            left++;
+        }
+        //subarray jo right side par end ho rha hai aur starting se lekr right end tak jo bhi elements hai, men= unhe include kr rhi hu
+        count += (right - left + 1); //imp to observe the '+' and '-' symbol
+    }
+    return count;
+}    
+
 
 int main() {
     vector<int> nums = {10, 5, 2, 6};
