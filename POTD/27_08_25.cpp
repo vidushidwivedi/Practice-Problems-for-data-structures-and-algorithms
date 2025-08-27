c0438add9dc811593a4ca6727336fe7f4aa2dc29
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-08-27 22:16:15 +0530

    Added today's POTD

---
+++ POTD/27_08_25.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> directions= {{1,1},{1,-1},{-1,-1},{-1,1}};
+
+int m, n;
+int t[501][501][4][2];
+
+int solve(int i, int j, int d, bool canTurn, int val, vector<vector<int>>& grid){
+        int i_= i+ directions[d][0];
+        int j_= j+ directions[d][1];
+
+        if (i_<0 || i_>=m || j_<0 || j_>=n || grid[i_][j_]!= val){
+            return 0;
+        }
+
+        if (t[i_][j_][d][canTurn] != -1){
+            return t[i_][j_][d][canTurn];
+        }
+
+        int result= 0;
+        int keepMoving =1+  solve (i_,j_, d, canTurn, val==2 ? 0 : 2, grid);
+        result= max(result, keepMoving);
+
+        if(canTurn== true){
+            int turnAndMove= max(keepMoving, 1+ solve(i_,j_, (d+1)%4, false, val==2 ? 0 : 2, grid));
+            result= max(result, turnAndMove);
+        }
+
+        return t[i_][j_][d][canTurn]= result;
+    }
+
+    int lenOfVDiagonal(vector<vector<int>>& grid) {
+        m= grid.size();
+        n= grid[0].size();
+        memset(t, -1, sizeof(t));
+
+        int result=0;
+
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+                if (grid[i][j]==1){
+                    for (int d=0; d<directions.size(); d++){
+                        result= max(result, 1+solve(i,j,d,true,2, grid));
+                    }
+                }
+            }
+        }
+        return result;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int rows, cols;
+    cin >> rows >> cols;
+
+    vector<vector<int>> grid(rows, vector<int>(cols));
+    for (int i=0; i<rows; i++){
+        for (int j=0; j<cols; j++){
+            cin >> grid[i][j];
+        }
+    }
+
+    cout << lenOfVDiagonal(grid) << "\n";
+    return 0;
+}
