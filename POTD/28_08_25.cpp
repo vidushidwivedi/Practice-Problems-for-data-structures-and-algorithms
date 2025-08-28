63f077d408b24fac15ef1d9c622434e6663d5e12
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-08-28 18:15:49 +0530

    Added the 2nd approach of potd

--- POTD/28_08_25.cpp
+++ POTD/28_08_25.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//Approach 1: using clear straightforward approach
+//tc: o(n^2log n)
+//sc: o(n)
 class Solution {
 public:
     int n;
@@ -48,6 +51,43 @@ public:
             sortDiagonal(0, col, grid, true);
         }
 
+        return grid;
+    }
+};
+
+//using approach 2: by maps
+//tc: o(n^2log n)
+//sc: o(n^2)
+class Solution {
+public:
+    
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        int n= grid.size();
+        unordered_map<int, vector<int>> mp;
+
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                int diag= i-j;
+                mp[diag].push_back(grid[i][j]);
+
+            }
+        }
+
+        for (auto &it : mp){
+            if (it.first>=0){//key
+                sort(begin(it.second), end(it.second));
+            }else {
+                sort(rbegin(it.second), rend(it.second));
+            }
+        }
+
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                int diag= i-j;
+                grid[i][j]=mp[diag].back();
+                mp[diag].pop_back();
+            }
+        }
         return grid;
     }
 };
