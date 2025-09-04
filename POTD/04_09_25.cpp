704557d5c8c2afaf53ec16528efa208825eccbfd
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-09-04 23:48:34 +0530

    Added today's POTD

---
+++ POTD/04_09_25.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        return abs(x-z)==abs(y-z)?0:2-(abs(x-z)<abs(y-z));
+    }
+};
