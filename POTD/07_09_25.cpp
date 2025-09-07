877a414f1b50e4155b56f92bb48636e717dd6fae
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-09-07 22:14:10 +0530

    Added second approach of today's POTD

--- POTD/07_09_25.cpp
+++ POTD/07_09_25.cpp
@@ -1,4 +1,26 @@
-//Approach-1 (iterate linear)
+// //Approach-1 (iterate linear)
+// //T.C : O(n) 
+// //S.C : O(1)
+// class Solution {
+// public:
+//     vector<int> sumZero(int n) {
+//         vector<int> result(n);
+
+//         int start = 1;
+//         int i = 0;
+//         while(i+1 < n) {
+//             result[i] = start;
+//             result[i+1] = -start;
+
+//             i+= 2;
+//             start++;
+//         }
+
+//         return result;
+//     }
+// };
+
+//Approach-2 (two pointer)
 //T.C : O(n) 
 //S.C : O(1)
 class Solution {
@@ -6,14 +28,16 @@ public:
     vector<int> sumZero(int n) {
         vector<int> result(n);
 
-        int start = 1;
         int i = 0;
-        while(i+1 < n) {
+        int j = n-1;
+        int start = 1;
+        while(i < j) {
             result[i] = start;
-            result[i+1] = -start;
-
-            i+= 2;
+            result[j] = -start;
             start++;
+
+            i++;
+            j--;
         }
 
         return result;
