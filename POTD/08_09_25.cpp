d8ab8601a8f64da94655b6816a4a324652a74303
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-09-08 18:30:26 +0530

    Modified the bf approach

--- POTD/08_09_25.cpp
+++ POTD/08_09_25.cpp
@@ -1,23 +1,28 @@
-class Solution {
-public:
+#include <bits/stdc++.h>
+using namespace std;
 
-    bool check (int num){
-        while (num){
-            if (num%10==0){
-                return false;
-            }
-            num/=10;
-        }
-        return true;
-    }
-    vector<int> getNoZeroIntegers(int n) {
-        for (int a=1; a<=n-1; a++){
-            int b= n-a;
-            if (check (a) && check (b)){
-                return {a,b};
-            }
-        }
-        return {};
+// class Solution {
+// public:
 
-    }
-};
+//     bool check (int num){
+//         while (num){
+//             if (num%10==0){
+//                 return false;
+//             }
+//             num/=10;
+//         }
+//         return true;
+//     }
+//     vector<int> getNoZeroIntegers(int n) {
+//         for (int a=1; a<=n-1; a++){
+//             int b= n-a;
+//             if (check (a) && check (b)){
+//                 return {a,b};
+//             }
+//         }
+//         return {};
+
+//     }
+// };
+//TC: nlogn
+//sc: o(1)
