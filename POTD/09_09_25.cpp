b217292013207ecb084529602ac4525a10a7f10d
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-09-09 23:55:31 +0530

    Added approach 3 of potd

--- POTD/09_09_25.cpp
+++ POTD/09_09_25.cpp
@@ -52,34 +52,74 @@ using namespace std;
 //Approach-2 - Bottom Up
 //T.C : O(n*(forget-delay))
 //S.C : O(n)
+// class Solution {
+// public:
+//     int M = 1e9+7;
+
+//     int peopleAwareOfSecret(int n, int delay, int forget) {
+//         vector<int> t(n+1);
+//         // t[day] = number of people who learn the secret on "day"
+
+//         t[1] = 1;
+
+//         for(int day = 2; day <= n; day++) {
+//             long long count = 0;
+//             for(int prev = day - forget+1; prev <= day - delay; prev++) {
+//                 if(prev > 0) {
+//                     count = (count + t[prev]) % M;
+//                 }
+//             }
+//             t[day] = count;
+//         }
+
+//         int result = 0;
+//         for(int day = n-forget+1; day <= n; day++) {
+//             if(day > 0) {
+//                 result = (result + t[day]) % M;
+//             }
+//         }
+
+//         return result;
+        
+//     }
+// };
+
+//Approach-3 - Bottom Up Optimised using sliding window
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
 public:
-    int M = 1e9+7;
+    int MOD = 1e9 + 7;
 
     int peopleAwareOfSecret(int n, int delay, int forget) {
-        vector<int> t(n+1);
+        vector<int> t(n + 1, 0);
         // t[day] = number of people who learn the secret on "day"
 
         t[1] = 1;
+        
+        int count = 0; 
 
-        for(int day = 2; day <= n; day++) {
-            long long count = 0;
-            for(int prev = day - forget+1; prev <= day - delay; prev++) {
-                if(prev > 0) {
-                    count = (count + t[prev]) % M;
-                }
+        for (int day = 2; day <= n; day++) {
+            // people who become eligible to share on this day
+            if (day - delay > 0) {
+                count = (count + t[day - delay]) % MOD;
             }
-            t[day] = count;
+            // people who forget on this day
+            if (day - forget > 0) {
+                count = (count - t[day - forget] + MOD) % MOD;
+            }
+            t[day] = count; //total number of people who will get to know today
         }
 
+
+        // Count the people who still remember on day n
         int result = 0;
-        for(int day = n-forget+1; day <= n; day++) {
-            if(day > 0) {
-                result = (result + t[day]) % M;
+        for (int day = n - forget + 1; day <= n; day++) {
+            if (day > 0) {
+                result = (result + t[day]) % MOD;
             }
         }
 
         return result;
-        
     }
 };
