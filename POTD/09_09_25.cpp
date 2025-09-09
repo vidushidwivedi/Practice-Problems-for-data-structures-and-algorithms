b8bd3a6a6f38f20ef10637a8b310d937e910f798
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-09-09 23:54:55 +0530

    Added approach 2 of potd

--- POTD/09_09_25.cpp
+++ POTD/09_09_25.cpp
@@ -49,3 +49,37 @@ using namespace std;
 //     }
 // };
 
+//Approach-2 - Bottom Up
+//T.C : O(n*(forget-delay))
+//S.C : O(n)
+class Solution {
+public:
+    int M = 1e9+7;
+
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        vector<int> t(n+1);
+        // t[day] = number of people who learn the secret on "day"
+
+        t[1] = 1;
+
+        for(int day = 2; day <= n; day++) {
+            long long count = 0;
+            for(int prev = day - forget+1; prev <= day - delay; prev++) {
+                if(prev > 0) {
+                    count = (count + t[prev]) % M;
+                }
+            }
+            t[day] = count;
+        }
+
+        int result = 0;
+        for(int day = n-forget+1; day <= n; day++) {
+            if(day > 0) {
+                result = (result + t[day]) % M;
+            }
+        }
+
+        return result;
+        
+    }
+};
