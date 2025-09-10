148ad122eebc57f6f8e01ffc533b520856fdf39f
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-09-10 23:58:55 +0530

    Added today's POTD

---
+++ POTD/10_09_25.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int M = 1e9+7;
+
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        vector<int> t(n+1);
+        // t[day] = number of people who learn the secret on "day"
+
+        t[1] = 1;
+
+        for(int day = 2; day <= n; day++) {
+            long long count = 0;
+            for(int prev = day - forget+1; prev <= day - delay; prev++) {
+                if(prev > 0) {
+                    count = (count + t[prev]) % M;
+                }
+            }
+            t[day] = count;
+        }
+
+        int result = 0;
+        for(int day = n-forget+1; day <= n; day++) {
+            if(day > 0) {
+                result = (result + t[day]) % M;
+            }
+        }
+
+        return result;
+        
+    }
+};
