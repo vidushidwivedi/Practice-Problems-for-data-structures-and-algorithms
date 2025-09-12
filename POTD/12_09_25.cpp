4f45294f899470f577dcbbdb5b49dedb2998b88c
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-09-12 23:58:21 +0530

    Added today's POTD

--- POTD/12_09_25.cpp
+++ POTD/12_09_25.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
+        //what we need to do is:
         // for (char &ch : s){
         //     if (ch== is a vowel){
         //         return true;
         //     }
         // }
         auto lambda= [] (char ch){
+            //first way in the below loc
             // return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
             return string ("aeiou").find(ch)!= string::npos; //check if the character is present or not from the above input
             //aeiou is the fixed length
