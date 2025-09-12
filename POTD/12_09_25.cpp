5c9b8003e9ebf8b3d2505e6e991c35f50350a8c1
vidushidwivedi <vidushidwivedi31@gmail.com>
2025-09-12 23:59:21 +0530

    Modified today's POTD

--- POTD/12_09_25.cpp
+++ POTD/12_09_25.cpp
@@ -10,9 +10,13 @@ public:
         auto lambda= [] (char ch){
             //first way in the below loc
             // return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
+
+            //another way in the below loc
             return string ("aeiou").find(ch)!= string::npos; //check if the character is present or not from the above input
             //aeiou is the fixed length
-          };
+          }; // remember to put ";"
+
+          
 
         return any_of(begin(s), end(s), lambda); //STL 
     }
